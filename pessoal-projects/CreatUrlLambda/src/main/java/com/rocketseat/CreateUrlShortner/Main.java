d4bcda74f40176015c74bb9a61b7b786999b6ef9
Francisca Tágila Lima da Silva <tagila.lima96@gmail.com>
2024-11-21 14:16:01 -0300

    Incluindo dependencias para capturar informacoes no body da requisicao

--- pessoal-projects/CreatUrlLambda/src/main/java/com/rocketseat/CreateUrlShortner/Main.java
+++ pessoal-projects/CreatUrlLambda/src/main/java/com/rocketseat/CreateUrlShortner/Main.java
@@ -2,15 +2,35 @@ package com.rocketseat.CreateUrlShortner;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
 
+    private static final ObjectMapper objectMapper = new ObjectMapper();
     @Override
-    public Map<String, String> handleRequest(Map<String, Object> stringObjectMap, Context context) {
-        return null;
+    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
+        String body = (String) input.get("body");
+
+        Map<String, String> bodyMap;
+        try{
+            bodyMap = objectMapper.readValue(body, Map.class);
+        }catch (Exception e){
+            throw  new RuntimeException("Error parsing JSON" + e.getMessage(), e);
+        }
+        String originalUrl = bodyMap.get("originalUrl");
+        String expirationTime = bodyMap.get("expirationTime");
+
+        String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
+
+        Map<String, String> response = new HashMap<String, String>();
+        response.put("statusCode", shortUrlCode);
+
+        return response;
     }
 }
