de7b877100bd29771eb66e82df73682a0a175d2e
Francisca Tágila Lima da Silva <tagila.lima96@gmail.com>
2024-11-23 01:12:27 -0300

    Utilizando s3 da AWS para armazenamento da URL

--- pessoal-projects/CreatUrlLambda/src/main/java/com/rocketseat/CreateUrlShortner/Main.java
+++ pessoal-projects/CreatUrlLambda/src/main/java/com/rocketseat/CreateUrlShortner/Main.java
@@ -2,20 +2,26 @@ package com.rocketseat.CreateUrlShortner;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
 
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    Region region = Region.US_EAST_1;
+    private final S3Client s3Client = S3Client.builder().region(region).build();
+
     @Override
     public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
-        String body = (String) input.get("body");
+        String body = input.get("body").toString();
 
         Map<String, String> bodyMap;
         try{
@@ -25,12 +31,33 @@ public class Main implements RequestHandler<Map<String, Object>, Map<String, Str
         }
         String originalUrl = bodyMap.get("originalUrl");
         String expirationTime = bodyMap.get("expirationTime");
+        long expirationTimeSec = Long.parseLong(expirationTime);
 
         String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
 
-        Map<String, String> response = new HashMap<String, String>();
+        UrlData urlData = new UrlData(originalUrl, expirationTimeSec);
+        try {
+            String urlDataJSON = objectMapper.writeValueAsString(urlData);
+
+            PutObjectRequest request = PutObjectRequest.builder().bucket("my-first-bucket-for-url-shortener-lambda").key(shortUrlCode + ".json").build();
+
+            s3Client.putObject(request, RequestBody.fromString(urlDataJSON));
+
+        }catch (Exception e) {
+            throw new RuntimeException("Error saving url data to S3" + e.getMessage(), e);
+        }
+        Map<String, String> response = new HashMap<>();
         response.put("statusCode", shortUrlCode);
 
         return response;
     }
+
+    /*public static void main(String[] args) {
+        Main main = new Main();
+        Map<String, Object> input = new HashMap<>();
+        input.put("body", "{\"originalUrl\": \"https://www.google.com\", \"expirationTime\": \"111111\"}");
+        Map<String, String> result = main.handleRequest(input, null);
+        System.out.println(result);
+
+    }*/
 }
